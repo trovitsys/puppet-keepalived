4ea5a01a8dba78da0b46c26c5d12fa3ddf299ad3
Javi Polo <javipolo@trovit.com>
2016-09-16 18:07:04 +0200

    Override systemd unit file on debian jessie

--- manifests/install.pp
+++ manifests/install.pp
@@ -4,5 +4,20 @@ class keepalived::install {
   package { $::keepalived::pkg_list:
     ensure => $::keepalived::pkg_ensure,
   }
+
+  # Update systemd unit file on debian jessie
+  if ( $::lsbmajdistrelease > 8 ) {
+    fail('We probably should use packages systemd unit file. Failing because of it')
+  } elsif ( $::lsbmajdistrelease == 8 ) {
+    file { '/etc/systemd/system/keepalived.service':
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
+        source  => 'puppet:///modules/keepalived/keepalived.systemd',
+        require => Package['keepalived'],
+        notify  => Service['keepalived'],
+    }
+  }
+
 }
 
