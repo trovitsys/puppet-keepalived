0a7ce14cf5368458ec605ed141a45f99e3290ca1
Javi Polo <javipolo@trovit.com>
2017-07-05 18:36:39 +0200

    Simple ordering fix from https://github.com/arioch/puppet-keepalived/commit/1fc34dd5d0c8990c4d02b0faeb413cf1e115d0f9

--- manifests/lvs/virtual_server.pp
+++ manifests/lvs/virtual_server.pp
@@ -159,7 +159,7 @@ define keepalived::lvs::virtual_server (
   concat::fragment { "keepalived.conf_lvs_virtual_server_${name}-footer":
     target  => "${::keepalived::config_dir}/keepalived.conf",
     content => "}\n",
-    order   => "250-${name}",
+    order   => "250-${name}-zzz",
   }
 
   if $collect_exported {
